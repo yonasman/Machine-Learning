63a1cc3192b5435773c77d44b771ccda6000a2e9
Yonas Negese M <yonasnegese250@gmail.com>
2024-11-06 23:01:18 +0300

    Solve second_largest element problem

--- List.py
+++ List.py
@@ -2,3 +2,14 @@
 nums = [1,2,3,4,5,6,7,8]
 even = [x for x in nums if x % 2 == 0]
 # print(even)
+# problem 1: find the second largest element
+def second_largest(nums):
+    unique_nums = set(nums)
+    n = len(unique_nums)
+    # if less than two unique elements
+    if n < 2:
+        return None
+    sorted_nums = sorted(unique_nums,reverse=True)
+    # return the second largest element
+    return sorted_nums[1]
+# print(second_largest([1,2,3,4,5]))
