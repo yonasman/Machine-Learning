686d77083e4da5b151998a8a5560da97e06279e2
Yonas Negese M <yonasnegese250@gmail.com>
2024-11-19 23:23:40 +0300

    Practice oop in python

--- OOP/oop.py
+++ OOP/oop.py
@@ -13,4 +13,27 @@ class Car:
 
 # object instances
 car1 = Car("Toyota","Corolla")
-car1.start_engine("corolla-1")
+# car1.start_engine("corolla-1")
+# encapsulation
+class Speaker:
+    # private attributes
+    def __init__(self, color, model):
+        self._color = color
+        self._model = model
+    # getters and setters
+    def get_color(self):
+        return self._color
+    def set_model(self, new_model):
+        self._model = new_model
+speaker1 = Speaker("red","toyota")
+# print(speaker1._color)
+# inheritance
+class SmartSpeaker(Speaker):
+    def __init__(self,color,model, voice_assistance):
+        super().__init__(color, model)
+        self.voice_assistant = voice_assistance
+        
+    def say_hello(self):
+        print(f'Hello {self.get_color()}')
+smartSpeaker1 = SmartSpeaker('yellow','toyota','eliza')
+# smartSpeaker1.say_hello()
