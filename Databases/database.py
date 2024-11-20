dbd4f077425a5c39f17d2b7bd3b8a46d81ddc99b
Yonas Negese M <yonasnegese250@gmail.com>
2024-11-20 23:03:15 +0300

    Woking with sqlite db started

--- Databases/database.py
+++ Databases/database.py
@@ -1,1 +1,10 @@
-print(dir())
+import sqlite3
+conn = sqlite3.connect("emaildb.sqlite")
+cur = conn.cursor()
+cur.execute("DROP TABLE IF EXISTS counts")
+cur.execute('CREATE TABLE counts(email TEXT, count INTEGER)')
+cur.execute("INSERT INTO counts(email, count) VALUES('yon@sql.com', 2)")
+
+
+
+
